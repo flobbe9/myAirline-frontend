3135659c882cbcf8c5a9fe0ac11aa192dbffb673
flobbe10 <florin.schikarski@tecfox.eu>
2023-02-14 17:26:40 +0100

    fix: put navbar and footer inside router tag

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/navBar/NavBar.tsx";
+import Footer from "./components/footer/Footer.tsx";
+import Home from "./components/home/Home.tsx";
+import Service from "./components/service/Service.tsx";
+import Jobs from "./components/jobs/Jobs.tsx";
+import Contact from "./components/contact/Contact.tsx";
+import About from "./components/about/About.tsx";
 
 
 function App() {
+
  	return (
 		<div className="App">
 			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<NavBar />}/>
-					<Route path="/service" element="adf"/>
-					<Route path="/jobs" element="Jobs"/>
-					<Route path="/contact" Contact />
-					<Route path="/about" About />
-				</Routes>
-			</BrowserRouter>
+				<NavBar />
 
-			<footer>
-				Footer...
-			</footer>
+				<Routes>
+					<Route path="/" element={<Home />}/>
+					<Route path="/service" element={<Service />}/>
+					<Route path="/jobs" element={<Jobs />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="/about" element={<About />} />
+				</Routes>
+
+				<Footer />
+			</BrowserRouter>
 		</div>
  	);
 }
