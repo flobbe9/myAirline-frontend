0a65440a04fc7775c14d041c1f93ef7b077611be
flobbe10 <florin.schikarski@tecfox.eu>
2023-02-26 23:53:06 +0100

    feat: add footer (more or less)

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import "./Footer.css";
+import { Link } from "react-router-dom";
 
 
 export default function Footer(props) {
 
     return (
         <div className="Footer-container">
-            <div className="Footer-item">
-                Footer
+            <div style={{display: "inline"}}>&copy;</div>
+            <div style={{display: "inline", paddingRight: "200px"}}>
+                myAirline
             </div>
+            <Link to="/contact" className="Footer-item">
+                Contact
+            </Link>
+            <Link to="/help" className="Footer-item">
+                Help
+            </Link>
+            <Link to="/service" className="Footer-item">
+                Service
+            </Link>
+            <Link to="/about" className="Footer-item">
+                About
+            </Link>
         </div>
     )
 }
