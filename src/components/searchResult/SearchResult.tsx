bbc69a2779b0a5837f26bd3a1b383ff73253aea3
flobbe10 <florin.schikarski@tecfox.eu>
2023-03-02 15:50:13 +0100

    feat: add functionality and style to SearchResult component

--- src/components/searchResult/SearchResult.tsx
+++ src/components/searchResult/SearchResult.tsx
@@ -1,50 +1,86 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./SearchResult.css";
+import { Link, useParams } from "react-router-dom";
+import sendHttpRequest from "../../helperMethods/fetch/fetch.ts";
 
 
-export default function SearchResult(props) {
+export default function SearchResult (props) {
+    // state
+    const [flightDetailsArr, setFlightDetailsArr] = useState(null);
+    const params = useParams();
+
+    useEffect(() => {
+        // fetch flight details
+        async function fetchFlightDetails() {
+            await sendHttpRequest("http://localhost:4001/flight/search", "post", "application/json", params)
+                .then(jsonResponse => setFlightDetailsArr(jsonResponse));
+        }
+
+        // prevent infinite loop
+        if (flightDetailsArr === null) 
+            fetchFlightDetails();
+    })
 
     return (
         <div className="SearchResult">
             <h1>Search results</h1>
 
             <div className="SearchResult-container">
-                <FlightResult className="SearchResult-item" flightDetails={{}}/>
+                {
+                    flightDetailsArr?
+                    (flightDetailsArr as []).map(flightDetails => (
+                        <FlightDetails className="SearchResult-item" flightDetails={flightDetails} />
+                    )):
+                    (<div style={{height:"1000px"}}></div>)
+                }
             </div>
         </div>
     )
 }
 
 
-function FlightResult(props) {
+function FlightDetails(props) {
 
     function handleClick() {
         
     }
 
+    const flightDetails = props.flightDetails;
+
     return (
         <div className={props.className} onClick={handleClick}>
-            <div className={props.className + "-departure"}>
-                <div>
-                    Hamburg
-                    <div className={props.className + "-arrow"}>
-                        {"->"}
+            <Link to="/searchResult/flightDetails">
+                <div className={props.className + "-departure"}>
+                    <div>
+                        {flightDetails.departureCity}
+                        <div className={props.className + "-arrow"}>
+                            {"->"}
+                        </div>
                     </div>
+                    <div>{flightDetails.departureTime} </div >
+                    <div className={props.className + "-details"}>27.02.23</div>
+                    <br />
+                    <div className={props.className + "-airline"}>RaynAir</div>
                 </div>
-                <div>10:00 </div >
-                <div className={props.className + "-details"}>27.02.23</div>
-                <br />
-                <div className={props.className + "-airline"}>RaynAir</div>
-            </div>
 
-            <div className={props.className + "-destination"}>
-                <div>München</div>
-                <div>12:45</div>
-                <div className={props.className + "-details"}>27.02.23</div>
-                <br />
-                <div className={props.className + "-price"}>34,00€</div>
-            </div>
-
+                <div className={props.className + "-destination"}>
+                    <div>{flightDetails.arrivalCity}</div>
+                    <div>{flightDetails.arrivalTime}</div>
+                    <div className={props.className + "-details"}>27.02.23</div>
+                    <br />
+                    <div className={props.className + "-price"}>34,00€</div>
+                </div>
+            </Link>
         </div>
     )
+}
+
+
+interface FlightDetailsWrapper {
+    departureCity: string
+    arrivalCity: string
+    departureTime: string
+    arrivalTime: string
+    departureDate: string
+    arrivalDate: string // calculate this one in the backend
 }
