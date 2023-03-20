846c9d8972a0fe2e1d2d9168227603eb4750c965
flobbe10 <florin.schikarski@tecfox.eu>
2023-03-20 23:33:33 +0100

    feat: add Login draft

--- src/components/userDetails/Login.tsx
+++ src/components/userDetails/Login.tsx
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Login.css";
+import { Link } from "react-router-dom";
+import { toggleColorOnclick } from "../../helperMethods/events/events";
 
 
 export default function Login(props) {
 
+    const [isFormValid, setIsFormValid] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
     const className = "Login";
+    
+    const submitButton = isFormValid? <Link to="">
+                                        <button id={className + "-submit"}>Submit</button>
+                                      </Link> : 
+                                      <button id={className + "-submit"}>Submit</button>
+
+    useEffect(() => {
+        const submitButton = document.getElementById(className + "-submit");
+
+        toggleColorOnclick((submitButton as HTMLElement), "gray");
+    }, [])
 
     return (
         <div className={className}>
             <h1>Login</h1>
 
             <div className={className + "-container"}>
-                <input type="text" />
+                <label htmlFor="email">Email</label><br />
+                <input className={className + "-input"} type="text" name="email" /><br /><br />
+
+                <label htmlFor="password">Password</label><br />
+                <input className={className + "-input"} type="password" name="password" /> <br /><br />
+
+                {submitButton}<br />
+                <div id={className + "-errorMessage"}>{errorMessage}</div>
             </div>
         </div>)
+}
+
+
+function handleSubmit() {
+
+    // validate email
+
+    // validate password
+
+    // fetch login request
+
+    // redirect or display error message
 }
